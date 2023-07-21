751bc9fdb26f16707ed0710525024cf3ff7224dc
Binaya Sharma <binaya.sharma@stackguardian.io>
2023-07-21 20:44:54 +0545

    Redis config, patch_schedule, and azurem storage account addition

--- main.tf
+++ main.tf
@@ -8,9 +8,41 @@ resource "azurerm_redis_cache" "redis-cache" {
   enable_non_ssl_port           = var.enable_non_ssl_port
   minimum_tls_version           = var.minimum_tls_version
   shard_count                   = var.sku_name == "Premium" ? var.cluster_shard_count : 0
-  private_static_ip_address = var.private_static_ip_address
-  subnet_id = var.subnet_id
+  private_static_ip_address     = var.private_static_ip_address
+  subnet_id                     = var.subnet_id
   public_network_access_enabled = var.public_network_access_enabled
   redis_version                 = var.redis_version
   zones                         = var.zones
+  redis_configuration {
+    aof_backup_enabled              = redis_configuration.value.aof_backup_enabled
+    aof_storage_connection_string_0 = redis_configuration.value.aof_storage_connection_string_0
+    aof_storage_connection_string_1 = redis_configuration.value.aof_storage_connection_string_1
+    enable_authentication           = redis_configuration.value.enable_authentication
+    maxmemory_reserved              = redis_configuration.value.maxmemory_reserved
+    maxmemory_delta                 = redis_configuration.value.maxmemory_delta
+    maxmemory_policy                = redis_configuration.value.maxmemory_policy
+    maxfragmentationmemory_reserved = redis_configuration.value.maxfragmentationmemory_reserved
+    rdb_backup_enabled              = redis_configuration.value.rdb_backup_enabled
+    rdb_backup_frequency            = redis_configuration.value.rdb_backup_frequency
+    rdb_backup_max_snapshot_count   = redis_configuration.value.rdb_backup_max_snapshot_count
+    rdb_storage_connection_string   = redis_configuration.value.rdb_storage_connection_string
+  }
+  lifecycle {
+    ignore_changes = [redis_configuration[0].rdb_storage_connection_string]
+  }
+  patch_schedule {
+    days_of_week       = patch_schedule.value.days_of_week
+    start_hour_utc     = patch_schedule.value.start_hour_utc
+    maintenance_window = patch_schedule.value.maintenance_window
+  }
+}
+
+resource "azurerm_storage_account" "redis_storage" {
+  name                     = var.storage_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.data_persistence_storage_account_tier
+  account_replication_type = var.data_persistence_storage_account_replication
+  account_kind             = "StorageV2"
+  min_tls_version          = "TLS1_2"
 }
--- variables.tf
+++ variables.tf
@@ -49,19 +49,61 @@ variable "public_network_access_enabled" {
 variable "redis_version" {
   description = "Redis version. Only major version needed. Valid values: 4, 6"
   type        = number
-  default     = 4
+  default     = 6
 
 }
 
 variable "private_static_ip_address" {
   description = "The Static IP Address to assign to the Redis Cache when hosted inside the Virtual Network. This argument implies the use of subnet_id. Changing this forces a new resource to be created."
-  type = string
-  deafult = null
+  type        = string
+  deafult     = null
 }
 
 variable "subnet_id" {
   description = "Only available when using the Premium SKU The ID of the Subnet within which the Redis Cache should be deployed. This Subnet must only contain Azure Cache for Redis instances without any other type of resources. Changing this forces a new resource to be created."
-  type = string
-  default = null
-  
+  type        = string
+  default     = null
+}
+
+variable "redis_additional_configuration" {
+  description = "Additional configuration for the Redis instance. Some of the keys are set automatically. See https://www.terraform.io/docs/providers/azurerm/r/redis_cache.html#redis_configuration for full reference."
+  type = object({
+    aof_backup_enabled              = optional(bool)
+    aof_storage_connection_string_0 = optional(string)
+    aof_storage_connection_string_1 = optional(string)
+    enable_authentication           = optional(bool)
+    maxmemory_reserved              = optional(number)
+    maxmemory_delta                 = optional(number)
+    maxmemory_policy                = optional(string)
+    maxfragmentationmemory_reserved = optional(number)
+    rdb_backup_enabled              = optional(bool)
+    rdb_backup_frequency            = optional(number)
+    rdb_backup_max_snapshot_count   = optional(number)
+    rdb_storage_connection_string   = optional(string)
+    notify_keyspace_events          = optional(string)
+  })
+  default = {}
+}
+
+variable "patch_schedules" {
+  description = "A list of Patch Schedule, Azure Cache for Redis patch schedule is used to install important software updates in specified time window."
+  default     = []
+  nullable    = false
+  type = list(object({
+    day_of_week        = string
+    start_hour_utc     = optional(string)
+    maintenance_window = optional(string)
+  }))
+}
+
+variable "data_persistence_storage_account_tier" {
+  description = "Replication type for the Storage Account used for data persistence."
+  type        = string
+  default     = "Premium"
+}
+
+variable "data_persistence_storage_account_replication" {
+  description = "Replication type for the Storage Account used for data persistence."
+  type        = string
+  default     = "LRS"
 }
